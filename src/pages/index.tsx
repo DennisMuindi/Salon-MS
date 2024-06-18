eca63264f6bfc9e67d3e8baf87cb8848cefab10a
DennisMuindi <mutukudennis7678@gmail.com>
2024-06-18 12:16:15 +0300

    deletions of spaces

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Login from "./login";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl"></h1>
+      <div className="bg-[#f0f5f7] w-full h-screen flex items-center justify-center">
+        <Login />
+      </div>
     </>
   );
 }
