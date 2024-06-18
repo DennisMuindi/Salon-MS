a6e6c5a4530b4412e1c48b18933dd251290d20fb
DennisMuindi <mutukudennis7678@gmail.com>
2024-06-18 12:17:11 +0300

    New page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import SideBar from "@/components/SideBar";
+import React from "react";
+
+export default function dashboard() {
+  return (
+    <div className="w-full bg-[#f0f5f7] h-screen flex items-start">
+      <SideBar />
+    </div>
+  );
+}
