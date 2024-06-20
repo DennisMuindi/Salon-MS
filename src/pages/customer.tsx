bdade9c0f5e4cee1d0462d17f1a1a97551ddc094
DennisMuindi <mutukudennis7678@gmail.com>
2024-06-21 01:58:28 +0300

    imported CustomerContent component

--- src/pages/customer.tsx
+++ src/pages/customer.tsx
@@ -1,10 +1,12 @@
 import SideBar from "@/components/SideBar";
+import CustomerContent from "@/components/customer/CustomerContent";
 import React from "react";
 
 export default function customer() {
   return (
-    <div>
+    <div className="w-full bg-[#f0f5f7] h-screen flex items-center justify-between">
       <SideBar />
+      <CustomerContent />
     </div>
   );
 }
