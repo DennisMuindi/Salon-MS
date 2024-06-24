996435e50489ff0a260c15489017b4323cf47672
DennisMuindi <mutukudennis7678@gmail.com>
2024-06-24 12:43:37 +0300

    rendered the schedulecontent component

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,10 +1,12 @@
 import SideBar from "@/components/SideBar";
+import ScheduleContent from "@/components/schedule/ScheduleMain";
 import React from "react";
 
 export default function schedule() {
   return (
     <div>
       <SideBar />
+      <ScheduleContent />
     </div>
   );
 }
